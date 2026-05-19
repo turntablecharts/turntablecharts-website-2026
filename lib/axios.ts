b2a2ed50d90f5850fd4ff5a896a4ad0263c260c9
simi <adegokesimi@gmail.com>
2026-05-19 06:48:08 +0100

    api key and roouting

--- lib/axios.ts
+++ lib/axios.ts
@@ -1,11 +1,26 @@
 import axios from "axios";
 
-const baseUrl = "https://turntablechartsapi.azurewebsites.net";
+const isBrowser = typeof window !== 'undefined';
+
+const baseUrl = isBrowser
+  ? "/api/ttc-proxy"
+  : (process.env.TTC_BACKEND_URL || "https://turntablechartsapi.azurewebsites.net");
 
 const TTCRequest = axios.create({
   baseURL: baseUrl,
   timeout: 180000,
 });
+
+if (!isBrowser) {
+  TTCRequest.interceptors.request.use((config) => {
+    const apiKey = process.env.TTC_API_KEY;
+    if (apiKey) {
+      config.headers = config.headers || {};
+      config.headers['x-api-key'] = apiKey;
+    }
+    return config;
+  });
+}
 
 const requestResponseHandler = (response: any) => {
   return response;
